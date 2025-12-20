2047a7cd54f838d5586a5d6e2cd14d9466be182a
Abhishekhack2909 <abhishektripathi317123@gmail.com>
2025-12-20 17:39:37 +0530

    feat: add user-related routes for password change, current user retrieval, account updates, and history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,18 @@ import {
   registeruser,
   loginuser,
   refreshAccessToken,
+  changePassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCover,
+  getUserChannelProfile,
+  watchhistory
+
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "crypto";
 const router = Router();
 router.route("/register").post(
   upload.fields([
@@ -27,8 +36,18 @@ router.route("/logout").post(verifyJWT, logoutuser);
 
 router.route("refreshAccessToken").post(refreshAccessToken)
 
+router.route("/change-password").post("verifyJWT", changePassword)
 
+router.route("/current-user").get(verifyJWT, getCurrentUser)
 
+router.route("/update-details").patch(verifyJWT,updateAccountDetails)
 
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar);
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"), updateUserCover )
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile );
+
+router.route("/history").get(verifyJWT, watchhistory)
 
 export default router;
