3922121ada38f02275a691021706287a7c7a6f53
Abhishekhack2909 <abhishektripathi317123@gmail.com>
2026-01-08 21:45:58 +0530

    feat: implement comment and subscription controllers with associated routes

---
+++ src/controllers/comments.controllers.js
@@ -0,0 +1,108 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+  // step 1: validate videoId
+  // step 2: fetch comments from db with pagination
+  // step 3: return response with comments
+  const isvalid=mongoose.Types.ObjectId.isvalid(videoId);
+      if(!isvalid){
+        throw new ApiError(400, "Invalid videoId");
+      }
+      else{
+        // step 2: fetch comments from db with pagination
+        const comments=await Comment.find({ videoId: videoId })
+        .skip((page-1)*limit)
+        .limit(limit)
+        .populate("commentedBy","username email");
+      }
+      //step 3  return the response
+      res
+      .status(200)
+      .json(new ApiResponse(200, "Comments fetched Succesfully ", comments));
+  
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add a comment to a video
+  // step 1:  get  videoId from req.params
+  //step 2: get comment text from req.body
+  // step 3: get userId from req.user_id(set by verifyJWT middleware)
+  //` step 4: create comment document and save to db
+  // step5: return the response with created comment
+
+  const { videoId } = req.params;
+  const { text } = req.body;
+  const userId = req.userId;
+  if (!text || text.trim() === "") {
+    throw new ApiError(400, "comment text cannnot be empty");
+  }
+  const newComment = await Comment.create({
+    videoId: videoId,
+    text: text,
+    commentedBy: userId,
+  });
+  res
+    .status(201)
+    .json(new ApiResponse(201, "comment added as successfully", newComment));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  // TODO: update a comment
+  // step 1: get commentId from req.params
+  // step 2: get new comment text from req.body
+  // step 3: get userId from req.user_id (set by verifyJWT middleware)
+  // step 4: find the comment in db
+  // step 5: check if the logged in user is the owner of the comment
+  // step 6: update the comment text and save
+  // step 7: return response with updated comment
+
+  const { commentId } = req.params;
+  const { text } = req.body;
+  const userId = req.userId;
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+  if (comment.commentedBy.toString() !== userId) {
+    throw new ApiError(403, "You are not authorized to update this comment");
+  }
+
+  comment.text = text;
+  await comment.save();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Comment updated successfully", comment));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+  // step 1: get commentId from req.params
+  // step 2: get userId from req.user_id (set by verifyJWT middleware)
+  // step 3: find the comment in db
+  // step 4: check if the logged in user is the owner of the comment
+  // step 5: delete the comment
+  // step 6: return response with success message
+  const { commentId } = req.params;
+  const userId = req.userId;
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+  if (comment.commentedBy.toString() !== userId) {
+    throw new ApiError(403, "You are not authorized to delete this comment");
+  }
+  await comment.deleteOne();
+
+  res.status(200).json(new ApiResponse(200, "Comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -9,16 +9,84 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    //step1: get subscriberId from req.user_id (set by verifyJWT middleware)
+    // step2: check if a subscription document already exists for the given channelId and subscriberId
+    // if exists then remove it (unsubscribe) else create a new subscription document (subscribe)
+    // return appropriate response
+
+    const subscriberId= req.userId;
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invlid channel id");
+    }      
+    const existedSusbscription= await Subscription.findOne({
+        channelId:channelId, 
+        subscriberId:subscriberId
+    })
+    if(existedSusbscription){
+        await Subscription.deleteOne({_id:existedSusbscription._id}) // delete the existing subscription
+    }else{
+        const newSubscription= await Subscription.create({
+            channelId:channelId,
+            subscriberid:subscriberId
+        })
+    }
+    //now reponse
+    res
+    .status(200)
+    .json(new ApiResponse(200, "Subscription toggled Succesfully ", null));
+
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    // TODO: get all subscribers of a channel
+    //step1: validate channelId
+    // step2: fetch subscribers from db
+    // step3: return response with subscribers list
+
+    //cheak for channel existence 
+    const channel=await User.findById(channelId);
+    if(!channel){
+        throw new ApiError(404, "channel not found")
+    }
+    
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channelId");
+    }
+    const subscribers= await Subscription.find({channelId:channelId})
+    .populate("subscriberId","username email");
+    //step3 return the response
+    res
+    .status(200)
+    .json(new ApiResponse(200, "Subscribers fetched Succesfully ", subscribers));
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    //step1: validate subscriberId
+    // step2: fetch subscribed channels from db
+    // step3: return response with channels list
+
+    // cheak for user existence
+    const user= await User.findById(subscriberId);
+    if(!user){
+        throw new ApiError(404, "User not fouund");
+    }
+    
+    if(!isValidObjectId(subscriberId)){
+        throw new ApiError(400, "Invalid SubscriberId");
+    }
+    const subscribedChannels= await Subscription.find({subscriberId:subscriberId})
+    .populate("channelId","username email");        
+    //step3 return the response
+    res
+    .status(200)
+    .json(new ApiResponse(200, "Subscribed channels fetched Succesfully ", subscribedChannels));
+
 })
 
 export {
